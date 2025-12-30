e94f5fc1c6a21c36891fde6221a9629571b040f1
fordmoneyroad-design <fordmoneyroad@gmail.com>
2025-12-30 09:41:05 -0500

    Add files via upload

---
+++ api/entities.js
@@ -0,0 +1,97 @@
+import { base44 } from './base44Client';
+
+
+export const Vehicle = base44.entities.Vehicle;
+
+export const Subscription = base44.entities.Subscription;
+
+export const Payment = base44.entities.Payment;
+
+export const Claim = base44.entities.Claim;
+
+export const DamageReport = base44.entities.DamageReport;
+
+export const SwapRequest = base44.entities.SwapRequest;
+
+export const ActivityLog = base44.entities.ActivityLog;
+
+export const Document = base44.entities.Document;
+
+export const TimeTracking = base44.entities.TimeTracking;
+
+export const Schedule = base44.entities.Schedule;
+
+export const Payroll = base44.entities.Payroll;
+
+export const FinancingOption = base44.entities.FinancingOption;
+
+export const CopartListing = base44.entities.CopartListing;
+
+export const SearchTracking = base44.entities.SearchTracking;
+
+export const CompanyEntity = base44.entities.CompanyEntity;
+
+export const MaintenanceAlert = base44.entities.MaintenanceAlert;
+
+export const DispatchRequest = base44.entities.DispatchRequest;
+
+export const JobPosting = base44.entities.JobPosting;
+
+export const CareerApplication = base44.entities.CareerApplication;
+
+export const ThemeSettings = base44.entities.ThemeSettings;
+
+export const PreEmploymentStatus = base44.entities.PreEmploymentStatus;
+
+export const VehicleInspection = base44.entities.VehicleInspection;
+
+export const VehicleFavorite = base44.entities.VehicleFavorite;
+
+export const AffiliateProgram = base44.entities.AffiliateProgram;
+
+export const PayrollRequest = base44.entities.PayrollRequest;
+
+export const PWAAnalytics = base44.entities.PWAAnalytics;
+
+export const StoreCredit = base44.entities.StoreCredit;
+
+export const ReturnRequest = base44.entities.ReturnRequest;
+
+export const BlogPost = base44.entities.BlogPost;
+
+export const PartnerCollaborator = base44.entities.PartnerCollaborator;
+
+export const PartnerAccessCode = base44.entities.PartnerAccessCode;
+
+export const CheckoutSession = base44.entities.CheckoutSession;
+
+export const DeliveryOption = base44.entities.DeliveryOption;
+
+export const Market = base44.entities.Market;
+
+export const Metafield = base44.entities.Metafield;
+
+export const TaxRegion = base44.entities.TaxRegion;
+
+export const TaxOverride = base44.entities.TaxOverride;
+
+export const ProductTaxCategory = base44.entities.ProductTaxCategory;
+
+export const BrandSettings = base44.entities.BrandSettings;
+
+export const PendingUser = base44.entities.PendingUser;
+
+export const ShippingQuote = base44.entities.ShippingQuote;
+
+export const EmployeeCategory = base44.entities.EmployeeCategory;
+
+export const EmployeeRole = base44.entities.EmployeeRole;
+
+export const CopartCredential = base44.entities.CopartCredential;
+
+export const CopartWatchlist = base44.entities.CopartWatchlist;
+
+
+
+// auth sdk:
+export const User = base44.auth;
